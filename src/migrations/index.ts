fd6eb28d4de728a36490facdcdff9a132f1c4312
Artiom Catruc <artiom.c@devstark.com>
2026-07-08 15:20:41 +0300

    chore: initial migrations from local schema

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260708_122041 from './20260708_122041';
+
+export const migrations = [
+  {
+    up: migration_20260708_122041.up,
+    down: migration_20260708_122041.down,
+    name: '20260708_122041'
+  },
+];
